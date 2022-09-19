951e6200319880d5e3f1eef969afa005955ad933
Malhotra2911 <101015733+Malhotra2911@users.noreply.github.com>
2022-09-19 11:46:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -25,26 +25,26 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled", "success");
-      // document.title = "TextUtils - Dark Mode";
+      // document.title = "WordlyWorld - Dark Mode";
       // setInterval(() => {
-      //   document.title = 'TextUtils is amazing mode'
+      //   document.title = 'WordlyWorld is amazing mode'
       // }, 2000);
       // setInterval(() => {
-      //   document.title = 'Install TextUtils now'
+      //   document.title = 'Install WordlyWorld now'
       // }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
-      // document.title = "TextUtils - Light Mode";
+      // document.title = "WordlyWorld - Light Mode";
     }
   };
   return (
     <>
-      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar title="WordlyWorld" aboutText="About WordlyWorld" /> */}
       {/* <Navbar /> */}
       <Router>
-        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Navbar title="WordlyWorld" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
           <Routes>
@@ -55,7 +55,7 @@ function App() {
               element={
                 <TextForm
                   showAlert={showAlert}
-                  heading="Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces"
+                  heading="Try WordlyWorld - Word Counter, Character Counter, Remove Extra Spaces"
                   mode={mode}
                 />
               }
